d64466772c0e3ace63a483987b15442e07c5341f
Komarova Irina <kmrva.is@gmail.com>
2020-02-12 00:52:28 +0900

    Выполняет 2 задачу 5 модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -13,11 +13,13 @@
  *
  * @constructor
  */
-function SortableTable(items) {
-  /**
-   * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
-   */
-  this.el = document.createElement('table');
+class SortableTable {
+  constructor(items, titles = ['Name', 'Age', 'Salary', 'City']) {
+    this.items = items;
+    this.titles = titles;
+    this.el = document.createElement('table');
+    this.render();
+  }
 
   /**
    * Метод выполняет сортировку таблицы
@@ -25,5 +27,48 @@ function SortableTable(items) {
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  sort(column, desc = false) {
+    let sortColumn = Object.keys(this.items[0])[column];
+
+    if (sortColumn) {
+      this.items = this.items.sort((a, b) =>{
+        if (desc){
+          return a[sortColumn] > b[sortColumn] ? -1 : 1;
+        }
+        else {
+          return a[sortColumn] > b[sortColumn] ? 1 : -1;
+        }
+
+      });
+
+      this.render();
+    }
+  }
+
+  render() {
+    this.el.innerHTML = '';
+
+    let thead = document.createElement('thead');
+    let tr = document.createElement('tr');
+    for (let i = 0 ; i < this.titles.length ; i++) {
+      let td = document.createElement('td');
+      td.innerHTML = this.titles[i];
+      tr.append(td);
+    }
+    thead.append(tr);
+    this.el.append(thead);
+
+    let tbody = document.createElement('tbody');
+    for (let i = 0 ; i < this.items.length ; i++) {
+      let tr = document.createElement('tr');
+      tbody.append(tr);
+
+      for (let key in this.items[i]) {
+        let td = document.createElement('td');
+        td.innerHTML = this.items[i][key];
+        tr.append(td);
+      }
+    }
+    this.el.append(tbody);
+  }
 }
