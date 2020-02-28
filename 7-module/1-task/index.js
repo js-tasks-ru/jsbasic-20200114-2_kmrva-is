5180ad478e524b644ab6630ef1a2ad540b45d56d
Komarova Irina <kmrva.is@gmail.com>
2020-02-29 00:48:53 +0900

    Выполняет задачу 1 модуль 7

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -5,5 +5,9 @@
  */
 
 function promiseClick(button) {
-
+  return new Promise(function (resolve) {
+    button.addEventListener('click', (event) => {
+      resolve(event);
+    }, {once: true});
+  });
 }
